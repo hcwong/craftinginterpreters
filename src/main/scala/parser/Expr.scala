fdfab25b625fa975a826f3b7e38facb7d227f755
Joshua Wong <joshwonghc@gmail.com>
2025-07-19 14:10:50 +0800

    Halfway through Chapter 7, can I add more static typing

--- src/main/scala/parser/Expr.scala
+++ src/main/scala/parser/Expr.scala
@@ -22,12 +22,53 @@ object Expr {
     def apply(value: Null) = NullLiteral
   }
 
+  private def isTruthy(obj: Any): Boolean = {
+    obj match {
+      case bool: Boolean => bool
+      case None          => false
+      case _             => true
+    }
+  }
+
   // TODO: Narrow down the type of operator. Not all tokens are operators
-  case class Unary(operator: TokenType, expr: Expr) extends Expr
+  case class Unary(operator: TokenType, expr: Expr) extends Expr {
+    def evaluateUnary: Any = {
+      val exprEvaluated = expr.evaluate
+
+      operator match {
+        case TokenType.BANG => !(isTruthy(exprEvaluated))
+        case TokenType.MINUS =>
+          exprEvaluated match {
+            case int: Int => -int
+            case _ =>
+              sys.error(
+                s"Attempted to apply token type '-' on non number value $exprEvaluated"
+              )
+          }
+        case tokenType =>
+          sys.error(
+            s"Unexpected operator type '$tokenType' while evaluating unary"
+          )
+      }
+    }
+  }
+
   case class Binary(leftExpr: Expr, operator: TokenType, rightExpr: Expr)
-      extends Expr
+      extends Expr {
+    def evaluateBinary: Any = {
+      val leftExprEvaluated = leftExpr.evaluate
+      val rightExprEvaluated = rightExpr.evaluate
+      
+      operator match {
+        case TokenType.
+      }
+    }
+  }
+  
   case class Ternary(condition: Expr, positive: Expr, negative: Expr)
       extends Expr
+  // Some parsers don't use separate Exprs for Parentheses, but Lox uses it to accurately
+  // evaluate LHS of assignment expressions
   case class Grouping(expr: Expr) extends Expr
 
   // TODO: Use Scala 3 typeclasses to implement implicit resolution
@@ -44,6 +85,16 @@ object Expr {
       case Ternary(condition, positive, negative) =>
         s"( Ternary: (${condition.print} ? ${positive.print} : ${negative.print}) )"
       case Grouping(expr) => s"( Grouping: ${expr.print} )"
+    }
+
+    // Fun idea: Try to make it somehow more typed? No to dynamically typed languages
+    def evaluate: Any = expr match {
+      case TrueLiteral          => true
+      case FalseLiteral         => false
+      case StringLiteral(value) => value
+      case IntLiteral(value)    => value
+      case NullLiteral          => None
+      case unary: Unary         => unary.evaluateUnary
     }
   }
 }
